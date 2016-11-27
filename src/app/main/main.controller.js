9e6e0eb2030d0f65a309562ba0ff3e4f88fb0eb2
ReddySrikar <srikar.nr@gmail.com>
2016-11-28 00:04:22 +0100

    Retrieval of messages from db.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,10 +2,19 @@ export class MainController {
   constructor ($http) {
     'ngInject';
     this.$http = $http;
+    this.getMessages();
 
   }
+    getMessages(){
+        var vm = this;
+        this.$http.get("http://localhost:5000/api/message").then(function(packet){
+            //console.log(packet);
+            vm.messages = packet.data;
+        });
+    };
+
     postMessage(){
         //console.log("The POST form value!!");
-        this.$http.post("http://localhost:5000/api/message", {msg:this.message});
-    }
+        this.$http.post("http://localhost:5000/api/message", {stmt:this.message});
+    };
 }
